819624580c3a7db81c55c27ab9c9cf6254a32885
Евгений Момотов <jn8momotov@gmail.com>
2018-11-20 02:20:14 +0300

    Initial Commit

---
+++ PhotoLibraryVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoLibraryVIPER
+//
+//  Created by Евгений on 20/11/2018.
+//  Copyright © 2018 Momotov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
